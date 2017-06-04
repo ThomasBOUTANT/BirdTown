70fd904489e5ff9c4d2ac1a0768247a0a7965597
ThomasBOUTANT <thomasboutant@orange.fr>
2017-06-04 18:51:33 +0200

    code without tests

--- CODE/Crime_a_BirdTown/BirdTown_Bibliotheque/monMain.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-int monMain();
-int factorielle(int const n);
-bool estUnCarreParfait(int const n);
--- CODE/Crime_a_BirdTown/Crime_a_BirdTown/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "../BirdTown_Bibliotheque/monMain.h"
-
-int main()
-{
-	return monMain();
-}
